9d8c2d590dd2e65e56a8c69b6a26d70f0c7e15ce
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2023-11-16 15:33:22 +0900

    * add Loading function.

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,22 @@
     <div class="container">
       <Title />
       <Form @submit-form="getWeather" />
-      <Results :results="results" />
+      <Results :results="results" v-if="!loading" />
+      <Loading v-if="loading" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import axios from 'axios';
 import Title from './components/Title.vue';
 import Form from './components/Form.vue';
 import Results from './components/Results.vue';
+import Loading from './components/Loading.vue';
 import './assets/base.css';
+
+const loading = ref(false);
 
 const results = reactive({
   country: '',
@@ -25,6 +29,7 @@ const results = reactive({
 });
 
 const getWeather = (city) => {
+  loading.value = true;
   axios
     .get(
       `http://api.weatherapi.com/v1/current.json?key=acab368a978240028be51642232108&q=${city}&aqi=no`,
@@ -35,6 +40,7 @@ const getWeather = (city) => {
         (results.temperature = res.data.current.temp_c),
         (results.conditionText = res.data.current.condition.text),
         (results.icon = res.data.current.condition.icon);
+      loading.value = false;
     });
 };
 </script>
